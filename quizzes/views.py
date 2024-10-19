48c43efbffd78400faa5ce5d878197bcc1d23e47
Brian Obot <brianobot9@gmail.com>
2024-10-19 21:52:40 +0100

    Updates

--- quizzes/views.py
+++ quizzes/views.py
@@ -23,6 +23,10 @@ from config.utils import seconds_until_next_day
 class QuestionViewSet(BaseView, viewsets.ViewSet):
     serializer_class = QuestionSerializer
 
+    def get_user_daily_question_reward(self, request: Request) -> int:
+        user_question_count = UserQuestion.objects.filter(user=request.user, created_at__date=timezone.now().date()).count()
+        return user_question_count * 700
+
     def get_user_daily_question_count(self, request: Request) -> int:
         user_id = request.user.id
         return cache.get(f"{user_id}_daily_question_count", 0)
@@ -33,6 +37,13 @@ class QuestionViewSet(BaseView, viewsets.ViewSet):
         updated_daily_question_count = current_daily_question_count + 1
         cache.set(f"{user_id}_daily_question_count", updated_daily_question_count, timeout=seconds_until_next_day())
         return updated_daily_question_count
+
+    def set_user_tried_daily_question(self, request: Request, question_id: str) -> int:
+        user_id = request.user.id
+        question_ids = cache.get(f"{user_id}_tried_question_ids", [])
+        question_ids.append(question_id)
+        cache.set(f"{user_id}_tried_question_ids", question_ids, timeout=seconds_until_next_day())
+        return True
 
     @extend_schema(
         operation_id="question_get",
@@ -45,14 +56,24 @@ class QuestionViewSet(BaseView, viewsets.ViewSet):
         user_answered_questions_ids = UserQuestion.objects.filter(user=request.user).values_list('question_id', flat=True)
 
         # Exclude these questions from the available pool
-        remaining_questions = Question.objects.exclude(id__in=user_answered_questions_ids)
+        tried_question_ids = cache.get(f"{request.user.id}_tried_question_ids", [])
+        remaining_questions = Question.objects.exclude(id__in=[
+            *user_answered_questions_ids,
+            *tried_question_ids,
+        ])
         
         if remaining_questions.exists():
             # Get a random question from the remaining pool
             random_question = remaining_questions[random.randint(0, remaining_questions.count() - 1)]
             serializer = self.serializer_class(random_question)
             question_count = self.set_user_daily_question_count(request)
-            return Response({"daily_question_count": question_count, **serializer.data})
+            self.set_user_tried_daily_question(request, random_question.id)
+            todays_reward = self.get_user_daily_question_reward(request)
+            return Response({
+                    "daily_question_count": question_count, 
+                    "daily_question_reward": todays_reward,
+                    **serializer.data
+                })
         else:
             return Response({"detail": "No more questions available for the user."}, status=404)
     
