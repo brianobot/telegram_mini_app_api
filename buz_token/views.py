35b8fb8c8921461fc28b0cd4c8fd9aed3bc9b24e
Brian Obot <brianobot9@gmail.com>
2024-10-23 14:56:56 +0100

    Fix bug on task claim endpoint

--- buz_token/views.py
+++ buz_token/views.py
@@ -11,22 +11,23 @@ from buz_token.models import Task
 
 class TaskViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = TaskSerializer
-    queryset = Task.objects.all()
+    queryset = Task.objects.all().reverse()
 
     @action(detail=True)
     def claim(self, request, *args, **kwargs):
         task = self.get_object()
+        user = request.user
         if not task.can_reward_user():
             msg = "Task is no longer available"
             raise serializers.ValidationError({"detail": msg})
 
-        if UserTask.objects.filter(task=task).exists():
+        if UserTask.objects.filter(task=task, user=user).exists():
             msg = "task already completed"
             raise serializers.ValidationError({"detail": msg})
         
-        if not task.evaluate_logic(request.user):
+        if not task.evaluate_logic(user):
             msg = "task not completed"
             raise serializers.ValidationError({"detail": msg})
         
-        task.reward_user(request.user)
+        task.reward_user(user)
         return Response(status=status.HTTP_202_ACCEPTED)
