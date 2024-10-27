8d4cd97cebc03dba4e84bf51d87bb6b93ad706e3
Brian Obot <brianobot9@gmail.com>
2024-10-27 10:07:24 +0100

    Add Media/Static url to url config

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import JsonResponse
 
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -27,4 +29,8 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),  # JSON Schema generation
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),  # Redoc UI
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"),name="swagger-ui",),  # Swagger UI
+] + [
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
+
