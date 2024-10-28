c224276f09b295eeba581137c7ef2da216a78e13
Brian Obot <brianobot9@gmail.com>
2024-10-28 08:36:49 +0100

    Updates

--- users/admin.py
+++ users/admin.py
@@ -6,3 +6,13 @@ from users.models import User
 @admin.register(User)
 class BuzzUserAdmin(admin.ModelAdmin):
     list_display = ['id', 'referrals', 'fullname', 'buz_tokens', 'created_at', 'updated_at']
+    actions = ['set_default_profile_pic']
+
+    @admin.action(description="Set Default profile image if it null")
+    def set_default_profile_pic(self, request, queryset):
+        default_profile_image = "profile_images/default_image.jpg"
+        for user in queryset:
+            user: User
+            if not user.profile_image:
+                user.profile_image = default_profile_image
+                user.save()
